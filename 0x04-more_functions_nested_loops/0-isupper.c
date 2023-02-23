a14cef3eb8681bdc12dc29d8abca90f27bb3cfda
ridwanademola <ridwanademola24@gmail.com>
2023-02-23 14:59:43 -0800

    the supper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -15,22 +15,3 @@ int _isupper(int c)
 	return (0);
 }
 
-OR
-
-
-#include "main.h"
-
-/**
- * _isupper - checks for uppercase character
- * @c: character to be checked
- *
- * Return: 1 for Success, 0 for Fail
- */
-int _isupper(int c)
-{
-	if (c >= 'A' && c <= 'Z')
-		return (1);
-	else
-		return (0);
-}
-
