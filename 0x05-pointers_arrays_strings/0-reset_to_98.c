adccf46660da4e693114cf98c6c0e46bec7a247d
ridwanademola <ridwanademola24@gmail.com>
2023-02-27 09:52:29 -0800

    task reset

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * reset_to_98n - take a pointer to an int parameter and updates
+ 
+ * the value it points to to 98
+ 
+ * @n: point to an int
+  
+ * Return: Nothing
+ 
+ */
+
+void reset_to_98(int *n);
+
+{
+	*n = 98;
+
+}
